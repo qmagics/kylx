c8a79ac58d071947d1490178beeae571664313e6
qmagics <949959378@qq.com>
2022-01-06 01:05:47 +0800

    添加面试题

---
+++ 面试题/caseConvert.js
@@ -0,0 +1,9 @@
+// 切换一个字符串中字母的大小写
+
+function caseConvert(str) {
+    return str.replace(/([a-z]*)([A-Z]*)/g, (m, s1, s2) => {
+        console.log(m,s1,s2)
+        return `${s1.toUpperCase()}${s2.toLowerCase()}`
+    })
+}
+console.log(caseConvert('aaa111AAA222CCCbbb333'))

94468872cc36ba1892d94d5eb50a943bbd41396d
fxd <949959378@qq.com>
2022-01-05 23:23:07 +0800

    Delete task directory

--- task/1.curry.js
+++
@@ -1,8 +0,0 @@
-/** 递归内层函数版本 */
-const curry = (fn) => {
-    const inner = (...args) => fn.length > args.length ? (...args2) => inner(...args, ...args2) : fn(...args);
-
-    return inner();
-}
-
-module.exports = curry;

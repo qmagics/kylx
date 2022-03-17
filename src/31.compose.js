921bc1392c573a35d9b4ecb0abb58da3cf251635
Ares <949959378@qq.com>
2022-03-17 17:33:11 +0800

    -

--- src/31.compose.js
+++ src/31.compose.js
@@ -8,4 +8,22 @@ const compose = (...fns) => {
     return fns.reduce((pre, cur) => (...args) => cur(pre(...args)));
 }
 
+function composeAsync(...args) {
+    if (args.length === 0) {
+        return (...args) => args;
+    }
+    if (args.length === 1) {
+        return args[0];
+    }
+
+    const init = args.pop();
+    return function (...args2) {
+        return args.reduceRight((pre, cur) => {
+            return pre.then(data => {
+                return cur(data);
+            })
+        }, Promise.resolve(init(...args2)));
+    }
+}
+
 module.exports = compose;
